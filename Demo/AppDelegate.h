e7d7fdb2d16bdcb82bdb513d398c66a4c8eb6b32
Sachin <sachin@sachins-macbook-pro.local>
2018-03-15 13:42:57 +0530

    Initial Commit

---
+++ Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by Sachin on 15/03/18.
+//  Copyright © 2018 Kinjal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
